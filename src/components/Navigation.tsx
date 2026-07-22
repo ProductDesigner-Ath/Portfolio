2b69abd5d7343dc854810f484cc1d54a232854d7
Adarshswagh <waghmareadarsh1432@gmail.com>
2026-07-22 12:31:33 +0530

    resume update

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -9,7 +9,7 @@ export default function Navigation() {
       </div>
       <a href="#" className="nav-logo">ATHARV</a>
       <div className="nav-right">
-        <a href="https://drive.google.com/file/d/1breHhutnikcF8RlMtDVYWgE5Oh_5RH3O/view?usp=sharing" className="nav-item" id="nav-resume-btn" target="">[Resume]</a>
+        <a href="/document/resume.pdf" className="nav-item" id="nav-resume-btn" target="_blank" rel="noopener noreferrer">[Resume]</a>
         <a href="mailto:hire@atharv.space" className="nav-item" id="nav-contact-btn">[Contact]</a>
       </div>
       <span className="nav-item" id="nav-proj-title"></span>
